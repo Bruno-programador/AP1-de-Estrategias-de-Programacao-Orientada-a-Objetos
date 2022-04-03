0be22a4144ed6eedb10bc6966af54031799647fc
Bruno-programador <62480894+Bruno-programador@users.noreply.github.com>
2022-04-03 16:37:24 -0300

    Add files via upload

---
+++ Ap1 - Copia/src/main/java/ap1/Controller/CRUDmotocicletaController.java
@@ -0,0 +1,57 @@
+package ap1.Controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.ArrayList;
+import java.util.List;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import ap1.Classes.Motocicleta;
+
+/**
+ *
+ * @author Bruno 5600
+ */
+public class CRUDmotocicletaController implements Initializable {
+
+    @FXML
+    public VBox telaMotocicleta;
+    
+    @FXML
+    public TextField motocicletaID;
+    @FXML
+    public TextField motocicletaModelo;
+    @FXML
+    public TextField motocicletaPreco;
+    
+    private Motocicleta motocicleta = new Motocicleta();
+
+    public static List<Motocicleta> listaMotocicleta = new ArrayList();
+    
+    
+    public void sair() {
+        ((Stage) telaMotocicleta.getScene().getWindow()).close();
+    }
+    
+    @FXML
+    public void pegarValores(){
+        motocicleta.setId(Integer.parseInt(motocicletaID.getText()));
+        motocicleta.setModelo(motocicletaModelo.getText());
+        motocicleta.setPreco(Double.valueOf(motocicletaPreco.getText()));
+
+        listaMotocicleta.add(motocicleta);
+                
+        sair();
+        ap1.Controller.RegMotocicletaController.AtualizarValoresMotocicleta();
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }    
+    
+}
